7a8c352e34309279f46a3aa2d6805f55585ba925
Pedro Cavalcante <pedrohen.fc@gmail.com>
2025-04-15 18:02:11 -0300

    lala

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ app = create_app()
 
 # TODO: formula of height,calores,weight
 # TODO: Quando o identificador tem um numero da bug
+# BUG : se colcoar uma dieta e depois colcoar outra com nome diferente ele nao atualzia
 if __name__ == "__main__":
     app.run(debug=True)
